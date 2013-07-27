548cad04d5e6d54aeae8737ea1f180f14ede5e54
Can Efeoglu <efeoglu@okcupidlabs.com>
2013-07-27 03:13:14 -0700

    lolo first commit yo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hulog::Application.config.secret_key_base = '196fad2586139999f8effe89a026d149f1d73db87820964ddec4e6c039221e6f15298a64343b93ce1c004d571aa571429b4ea88b1bc0fa450303b7fda14f83b4'
